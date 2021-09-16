f903a17b4a2fe5442e3db287e35e84a085fa34d0
aledark <maria.aguiar@correounivalle.edu.co>
2021-09-16 15:10:18 +0000

    Curl addings

--- app/app.py
+++ app/app.py
@@ -48,13 +48,18 @@ def index():
         tasks = Todo.query.order_by(Todo.date_created).all() 
         return render_template('index.html', tasks=tasks)
 
-@app.route('/delete/<int:id>', methods=['GET', 'DELETE'])
-def delete(id): 
+@app.route('/delete/<int:id>', methods=['GET','DELETE'])
+def delete(id):
     task_to_delete = Todo.query.get_or_404(id)
     try:
         db.session.delete(task_to_delete) 
         db.session.commit()
-        return redirect('/')
+        if request.method == 'DELETE':
+            return "Deleted!!"
+        elif request.method == 'GET':
+            return redirect('/')
+        else:
+            render_template('index.html', tasks=tasks)
     except:
         return "There was a problem deleting that task"
 
